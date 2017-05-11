fee605965ce1ed46977f9586a7c1247e1e8a90df
Clovel <cd.clovel19@gmail.com>
2017-05-11 12:27:19 +0200

    Suite Rapport : Partie 1 et debut partie 2

--- SOFTWARE/Sources/OLED_power_on.c
+++ SOFTWARE/Sources/OLED_power_on.c
@@ -20,12 +20,6 @@ void init_OLED(void)
 	send_char('2');
 	send_nl();
 
-	/* -------------------------- */
-	/* WARNING : L'ordre des deux */
-	/* étapes suivantes est p-e a */
-	/* inverser (a tester)        */
-	/* -------------------------- */
-
 	// Bring the Vcc Enable (pin 9) logic low 
 	// VCC controlé par VCCEN ie VBATC
 	OLED_VBATC = CLEAR;
@@ -44,24 +38,26 @@ void init_OLED(void)
 	OLED_RES = CLEAR;
 	wait_us(15); // On majore 3us par 5us
 	OLED_RES = SET;
+	send_char('5');
+	send_nl();
 
 	// Wait for the CLEAR display operation to complete; 
 	// this takes a maximum of 3 us to complete
 	wait_us(15); // On majore 3us par 5us
-	send_char('5');
+	send_char('6');
 	send_nl();
 
 	// Enable the driver IC to accept commands by sending the unlock command over SPI – 0xFD, 0x12
 	OLED_spi_sendchar(0xFD); //MISO = 0xFD;
 
 	OLED_spi_sendchar(0x12); //MISO = 0x12;
-	send_char('6');
+	send_char('7');
 	send_nl();
 
 
 	// Send the display off command – 0xAE
 	OLED_spi_sendchar(0xAE); // Display Off (Sleep Mode)
-	send_char('7');
+	send_char('8');
 	send_nl();
 
 
@@ -94,47 +90,48 @@ void init_OLED(void)
 						 A[1]=0, RAM Column 0 to 95 maps to Pin Seg (SA,SB,SC) 95 to 0
 						 A[0]=0, Horizontal address increment*/
 	}
-	send_char('8');
+	send_char('9');
 	send_nl();
 
 	// Set the Display start Line – 0xA1, 0x00 to set the starting line in the upper left corner
 	OLED_spi_sendchar(0xA1); 
 	OLED_spi_sendchar(0x00); // Start line register at 0. 
-	send_char('9');
+	send_char('1');
+	send_char('0');
 	send_nl();
 
 	// Set the Display Offset – 0xA2, 0x00 for no offset
 	OLED_spi_sendchar(0xA2);
 	OLED_spi_sendchar(0X00); // No offset
 	send_char('1');
-	send_char('0');
+	send_char('1');
 	send_nl();
 
 	// Make it a normal display – 0xA4 for a normal display, i.e. no funky settings like all pixels being on/off or the color inverted
 	OLED_spi_sendchar(0xA4); // Normal Display
 	send_char('1');
-	send_char('1');
+	send_char('2');
 	send_nl();
 
 	// Set the Multiplex Ratio – 0xA8, 0x3F to enable all of the common pins
 	OLED_spi_sendchar(0xA8);
 	OLED_spi_sendchar(0x3F);
 	send_char('1');
-	send_char('2');
+	send_char('3');
 	send_nl();
 
 	// Set Master Configuration – 0xAD, 0x8E to force bit 0 of this register to be a zero after a CLEAR to make the datasheet happy
 	OLED_spi_sendchar(0xAD);
 	OLED_spi_sendchar(0x8E); //A[0]=0b, Select external VCC supply
 	send_char('1');
-	send_char('3');
+	send_char('4');
 	send_nl();
 
 	// Set Power Saving Mode – 0xB0, 0x0B to disable power saving mode
 	OLED_spi_sendchar(0xB0);
 	OLED_spi_sendchar(0x0B); // Disable power saving mode
 	send_char('1');
-	send_char('4');
+	send_char('5');
 	send_nl();
 
 	// Set Phase Length – 0xB1, 0x31 to set the period of the display clock (DCLK) 
@@ -145,7 +142,7 @@ void init_OLED(void)
 	OLED_spi_sendchar(0xB1);
 	OLED_spi_sendchar(0x31);
 	send_char('1');
-	send_char('5');
+	send_char('6');
 	send_nl();
 
 
@@ -159,7 +156,7 @@ void init_OLED(void)
 	OLED_spi_sendchar(0xB3);
 	OLED_spi_sendchar(0xF0);
 	send_char('1');
-	send_char('6');
+	send_char('7');
 	send_nl();
 
 
@@ -169,7 +166,7 @@ void init_OLED(void)
 	OLED_spi_sendchar(0x8A);
 	OLED_spi_sendchar(0x64);
 	send_char('1');
-	send_char('7');
+	send_char('8');
 	send_nl();
 
 
@@ -179,7 +176,7 @@ void init_OLED(void)
 	OLED_spi_sendchar(0x8B);
 	OLED_spi_sendchar(0x78);
 	send_char('1');
-	send_char('8');
+	send_char('9');
 	send_nl();
 
 	// Set the Second Pre-Charge Speed of Color C¹ 
@@ -187,8 +184,8 @@ void init_OLED(void)
 	// blue by default, to a target driving voltage
 	OLED_spi_sendchar(0x8C);
 	OLED_spi_sendchar(0x64);
-	send_char('1');
-	send_char('9');
+	send_char('2');
+	send_char('0');
 	send_nl();
 
 
@@ -199,7 +196,7 @@ void init_OLED(void)
 	OLED_spi_sendchar(0xBB);
 	OLED_spi_sendchar(0x3A);
 	send_char('2');
-	send_char('0');
+	send_char('1');
 	send_nl();
 
 	// Set the VCOMH Deselect Level 
@@ -208,7 +205,7 @@ void init_OLED(void)
 	OLED_spi_sendchar(0xBE);
 	OLED_spi_sendchar(0x3E);
 	send_char('2');
-	send_char('1');
+	send_char('2');
 	send_nl();
 
 	// Set Master Current Attenuation Factor 
@@ -219,7 +216,7 @@ void init_OLED(void)
 	OLED_spi_sendchar(0x87);
 	OLED_spi_sendchar(0x06);
 	send_char('2');
-	send_char('2');
+	send_char('3');
 	send_nl();
 
 	// Set the Contrast for Color A 
@@ -230,7 +227,7 @@ void init_OLED(void)
 	OLED_spi_sendchar(0x81);
 	OLED_spi_sendchar(0xFF);
 	send_char('2');
-	send_char('3');
+	send_char('4');
 	send_nl();
 
 	// Set the Contrast for Color A 
@@ -241,7 +238,7 @@ void init_OLED(void)
 	OLED_spi_sendchar(0x82);
 	OLED_spi_sendchar(0xFF);
 	send_char('2');
-	send_char('4');
+	send_char('5');
 	send_nl();
 
 	// Set the Contrast for Color A
@@ -252,20 +249,20 @@ void init_OLED(void)
 	OLED_spi_sendchar(0x83);
 	OLED_spi_sendchar(0xFF);
 	send_char('2');
-	send_char('5');
+	send_char('6');
 	send_nl();
 
 	// Disable Scrolling – 0x2E
 	OLED_spi_sendchar(0x2E);
 	send_char('2');
-	send_char('6');
+	send_char('7');
 	send_nl();
 
 	// Enable Fill for rectangle
 	OLED_spi_sendchar(0x26);
 	OLED_spi_sendchar(0x01);
 	send_char('2');
-	send_char('7');
+	send_char('8');
 	send_nl();
 
 	// Clear the screen
@@ -278,14 +275,14 @@ void init_OLED(void)
 	OLED_spi_sendchar(0x5F);
 	OLED_spi_sendchar(0x3F);
 	send_char('2');
-	send_char('8');
+	send_char('9');
 	send_nl();
 
 	// Bring VCCEN (pin 9) logic high and wait 25 ms
 	OLED_VBATC = SET;
 	wait_ms(125);
-	send_char('2');
-	send_char('9');
+	send_char('3');
+	send_char('0');
 	send_nl();
 
 	// Turn the display on (0xAF) 
@@ -293,7 +290,7 @@ void init_OLED(void)
 	OLED_spi_sendchar(0xAF);
 	wait_ms(200);
 	send_char('3');
-	send_char('0');
+	send_char('1');
 	send_nl();
 
 	// Note¹ – when changing any of the second pre-charge speeds, all three registers must be updated sequentially with 6 bytes total (address A, value A, address B, value B, address C, value C), even if their values do not change.
